3c0f532e95c428bc51da7ee40fb41ed20f659cef
Evgeny Stepanischev <imbolk@gmail.com>
2024-07-18 09:38:20 +0300

    Split the code into functions

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,19 @@ pub fn shape(
     let buf_u8: Vec<u8> = buffer.glyphs.iter().map(|g| g.codepoint as u8).collect();
     let str_buf = String::from_utf8_lossy(&buf_u8);
     
+    let morse_glyphs = create_morse_glyphs(&str_buf);
+    
+    buffer.glyphs = morse_glyphs;
+    
+    for glyph in &mut buffer.glyphs {
+        glyph.codepoint = font.get_glyph(glyph.codepoint, 0);
+        glyph.x_advance = font.get_glyph_h_advance(glyph.codepoint);
+    }
+    
+    1
+}
+
+fn create_morse_glyphs(input_str: &str) -> Vec<Glyph> {
     // Morse code mappings
     let morse_code_map: Vec<&str> = vec![
         ".-",    // A
@@ -56,7 +69,7 @@ pub fn shape(
     ];
     
     let mut morse_glyphs = Vec::new();
-    for (i, c) in str_buf.chars().enumerate() {
+    for (i, c) in input_str.chars().enumerate() {
         let idx = match c {
             'A'..='Z' => (c as usize) - ('A' as usize),
             '0'..='9' => ('Z' as usize - 'A' as usize + 1) + (c as usize) - ('0' as usize),
@@ -65,38 +78,36 @@ pub fn shape(
 
         if idx < morse_code_map.len() {
             let morse_str = morse_code_map[idx];
-            if i > 0 {
-                // Insert space as a new glyph
-                morse_glyphs.push(Glyph {
-                    codepoint: ' ' as u32,
-                    flags: 0,
-                    x_advance: 0,
-                    y_advance: 0,
-                    cluster: morse_glyphs.len() as u32,
-                    x_offset: 0,
-                    y_offset: 0,
-                });
-            }
-            for (ix, x) in morse_str.chars().enumerate() {
-                morse_glyphs.push(Glyph {
-                    codepoint: x as u32,
-                    flags: 0,
-                    x_advance: 0,
-                    y_advance: 0,
-                    cluster: (morse_glyphs.len() + ix) as u32,
-                    x_offset: 0,
-                    y_offset: 0,
-                });
-            }
+            push_morse_glyphs(&mut morse_glyphs, morse_str, i > 0);
         }
     }
     
-    buffer.glyphs = morse_glyphs;
-    
-    for glyph in &mut buffer.glyphs {
-        glyph.codepoint = font.get_glyph(glyph.codepoint, 0);
-        glyph.x_advance = font.get_glyph_h_advance(glyph.codepoint);
+    morse_glyphs
+}
+
+fn push_morse_glyphs(morse_glyphs: &mut Vec<Glyph>, morse_str: &str, insert_space: bool) {
+    if insert_space {
+        // Insert space as a new glyph
+        morse_glyphs.push(Glyph {
+            codepoint: ' ' as u32,
+            flags: 0,
+            x_advance: 0,
+            y_advance: 0,
+            cluster: morse_glyphs.len() as u32,
+            x_offset: 0,
+            y_offset: 0,
+        });
     }
-    
-    1
+
+    for (ix, x) in morse_str.chars().enumerate() {
+        morse_glyphs.push(Glyph {
+            codepoint: x as u32,
+            flags: 0,
+            x_advance: 0,
+            y_advance: 0,
+            cluster: (morse_glyphs.len() + ix) as u32,
+            x_offset: 0,
+            y_offset: 0,
+        });
+    }
 }
